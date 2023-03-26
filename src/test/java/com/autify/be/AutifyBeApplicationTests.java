b020b99e860272d9aff439a9aa75c93ee0d27313
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2023-03-26 09:06:09 +0700

    Initialize project
    Add introduction, description

---
+++ src/test/java/com/autify/be/AutifyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autify.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutifyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
