8b30889441df84087a1d149b7cf5d5280dec3ecc
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2023-05-07 14:22:43 +0700

    Update spring doc api

--- src/main/java/com/autify/be/AutifyBeApplication.java
+++ src/main/java/com/autify/be/AutifyBeApplication.java
@@ -1,11 +1,12 @@
 package com.autify.be;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class AutifyBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AutifyBeApplication.class, args);
     }
